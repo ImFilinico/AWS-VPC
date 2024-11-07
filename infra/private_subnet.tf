e58af46f39294a3289e772c33f9fae0ae9690c73
Nicolas Mangilit <nico.mangilit@outlook.com>
2024-11-06 18:09:37 -0600

    add private subnets

---
+++ infra/private_subnet.tf
@@ -0,0 +1,15 @@
+variable "private_subnet_cidrs" {
+ type        = list(string)
+ description = "Private Subnet CIDR values"
+ default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
+resource "aws_subnet" "private_subnets" {
+ count      = length(var.private_subnet_cidrs)
+ vpc_id     = aws_vpc.main.id
+ cidr_block = element(var.private_subnet_cidrs, count.index)
+ 
+ tags = {
+   Name = "Private Subnet ${count.index + 1}"
+ }
+}
