b5b4aab3e8230480faa5acbf3a0d42a5e5307959
Nicolas Mangilit <nico.mangilit@outlook.com>
2024-11-07 22:37:34 -0600

    added route table association for public subnets

--- infra/public_subnet.tf
+++ infra/public_subnet.tf
@@ -8,3 +8,9 @@ resource "aws_subnet" "public_subnets"{
         Name = "Public Subnet ${count.index+1}"
     }
 }
+
+resource "aws_route_table_association" "public_subnet_asso" {
+    count = length(var.public_subnet_cidrs)
+    subnet_id = element(aws_subnet.public_subnets[*].id, count.index)
+    route_table_id = aws_route_table.second_rt.id
+}
